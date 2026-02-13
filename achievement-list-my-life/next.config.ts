3626a6e3f5711b31f04ee340fa34619d4822d76d
Lyuboslav <222031@buas.nl>
2026-02-13 19:10:53 +0100

    [Test] add vercel.json

--- achievement-list-my-life/next.config.ts
+++ achievement-list-my-life/next.config.ts
@@ -1,8 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
-  }
+  },
 }
 
-export default nextConfig
+export default nextConfig;
